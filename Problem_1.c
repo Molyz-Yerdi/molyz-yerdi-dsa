083f988d43086d1e8e0348e4a591b491241041f6
Deni Irizbaev <deni.engenoy@gmail.com>
2025-01-23 05:55:31 +0100

    This commit adds the solution to the first problem

---
+++ Problem_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int Number(int a)
+{
+    return (1000 - 1) / a;
+}
+
+int SumN(int a)
+{
+    int n = Number(a);
+    int S = (2 * a + a * (n - 1)) * n / 2;
+    return S;
+}
+
+int main (void)
+{
+    int Sum = SumN(3) + SumN(5) - SumN(15);
+    printf("Sum of all the multiples of 3 or 5 below 1000 is %d\n", Sum);
+    return 0;
+}
