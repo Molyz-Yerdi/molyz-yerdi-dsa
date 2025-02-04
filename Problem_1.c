cc8d697d810c347856e2c8bd81a061cfe9f23459
Deni Irizbaev <deni.engenoy@gmail.com>
2025-02-04 16:08:25 +0100

    This commit added new Problem_3 version

--- Problem_1.c
+++ Problem_1.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.</p>
+*/
 
 int Number(int a)
 {
     return (1000 - 1) / a;
 }
 
-
 int SumN(int a)
 {
     int n = Number(a);
     int S = (2 * a + a * (n - 1)) * n / 2;
+
     return S;
 }
-
 
 int main(void)
 {
     int Sum = SumN(3) + SumN(5) - SumN(15);
     printf("%d\n", Sum);
+
     return 0;
 }
--- Problem_3.c
+++ Problem_3.c
@@ -1,7 +1,117 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+// Function for fast modular exponentiation (a^b % mod)
+long long ModPow(long long base, long long exp, long long mod)
+{
+    long long result = 1;
+    base = base % mod;
+    
+    while (exp > 0) 
+    {
+        if (exp % 2 == 1)
+        {
+            result = (result * base) % mod;
+        }
+        exp = exp >> 1;
+        base = (base * base) % mod;
+    }
+    return result;
+}
 
+// One round of the Miller-Rabin test
+int MillerRabin(long long n, long long a)
+{
+    long long d = n - 1;
+    int r = 0;
+
+    while (d % 2 == 0)
+    {
+        d /= 2;
+        r++;
+    }
+
+    long long x = ModPow(a, d, n);
+    if (x == 1 || x == n - 1)
+    {
+        return 1;
+    }
+
+    for (int i = 0; i < r - 1; i++)
+    {
+        x = (x * x) % n;
+        if (x == n - 1)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+// Function to check if a number is prime using the Miller-Rabin test
+int IsPrime(long long n, int iterations)
+{
+    if (n < 2) { return 0; }
+    if (n == 2 || n == 3) { return 1; }
+    if (n % 2 == 0) { return 0; }
+
+    int test_bases[5] = {2, 3, 5, 7, 11};
+    for (int i = 0; i < iterations; i++)
+    {
+        long long a = test_bases[i % 5];
+        if (a >= n) 
+        {
+            break;
+        }
+        if (!MillerRabin(n, a))
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+// Finding the largest prime factor
+long long PrimeFactor(long long N)
+{
+    long long largest_factor = 1;
+
+    // Remove all factors of 2
+    while (N % 2 == 0)
+    {
+        largest_factor = 2;
+        N /= 2;
+    }
+
+    // Check odd divisors from 3 to sqrt(N)
+    for (long long i = 3; i * i <= N; i += 2)
+    {
+        while (N % i == 0)
+        {
+            if (IsPrime(i, 5))  // Check if i is prime
+            {
+                largest_factor = i;
+            }
+            N /= i;
+        }
+    }
+
+    // If there is any number left > 1, it is prime
+    if (N > 1 && IsPrime(N, 5))
+    {
+        largest_factor = N;
+    }
+
+    return largest_factor;
+}
+
+// Main function
 int main(void)
 {
+    long long N = 600851475143LL;
+    long long largest_factor = PrimeFactor(N);
+    printf("%lld\n", largest_factor);
     return 0;
 }
