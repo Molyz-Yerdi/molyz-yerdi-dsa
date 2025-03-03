51f1a2c6af9f50a37d0b6b3a3358799e527b1871
Deni Irizbaev <deni.engenoy@gmail.com>
2025-03-03 14:24:11 +0100

    Update for Roman2Int.c

--- Roman2Int.c
+++ Roman2Int.c
@@ -1,45 +1,63 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #define SIZE 50
 
 int roman2int(char *s);
 void int2roman(char *result, int number, size_t size);
+int is_integer(char *argv);
 
-const struct {
-    const char *symbol;
-    int value;
-} roman_map[13] = {
-    {"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400}, {"C", 100},
-    {"XC", 90},  {"L", 50},   {"XL", 40}, {"X", 10},   {"IX", 9},
-    {"V", 5},    {"IV", 4},   {"I", 1}
-};
-
-
-int main(void)
+int main(int argc, char *argv[])
 {
+    if (is_integer(argv[1]))
+    {
+        char result[SIZE];
+        int number = atoi(argv[1]);
+        int2roman(result, number, SIZE);
+        printf("%s\n", result);
+    }
+    else
+    {
+        printf("%d\n", roman2int(argv[1]));
+    }
+
     // char s[] = "LCD";
     // printf("%d\n", roman2int(s));
-
-    char result[SIZE];
-    int number = 1994;
-    int2roman(result, number, SIZE);
-    printf("%s\n", result);
+    // char result[SIZE];
+    // int number = 1994;
+    // int2roman(result, number, SIZE);
+    // printf("%s\n", result);
 
     return 0;
 }
 
+int is_integer(char *arg)
+{
+    int i = 0;
+    while(arg[i] != '\0')
+    {
+        if(!isdigit(arg[i]))
+        {
+            return 0;
+        }
+        i++;
+    }
+    return 1;    
+}
+
+const struct
+{
+    const char *symbol;
+    int value;
+}
+roman_map[13] = {
+    {"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400}, {"C", 100},
+    {"XC", 90},  {"L", 50},   {"XL", 40}, {"X", 10},   {"IX", 9},
+    {"V", 5},    {"IV", 4},   {"I", 1}};
+
 void int2roman(char *result, int number, size_t size)
 {
-    const struct
-    {
-        const char *symbol;
-        int value;
-    }
-    roman_map[13] = {{"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400}, {"C", 100},
-                     {"XC", 90},  {"L", 50},   {"XL", 40}, {"X", 10},   {"IX", 9},
-                     {"V", 5},    {"IV", 4},   {"I", 1}};
-
     for (int i = 0; number > 0; ++i)
     {
         while (number >= roman_map[i].value)
